d787ddcb3b779857814e00317ab4ede2b5f4ce94
Michael <mckeever02@gmail.com>
2020-04-09 23:15:15 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,50 @@
 module.exports = {
+  important: true,
   theme: {
-    extend: {}
+    fontFamily: {
+      display: ['GT Sectra', 'serif'],
+      body: ['GT Haptik', 'sans-serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '3.5rem',
+      '7xl': '3.875rem',
+      '8xl': '4rem',
+    },
+    extend: {
+      colors: {
+        darkGreen: '#194148',
+        darkerGreen: '#16383E',
+        darkestGreen: '#132D32',
+        sanJuan: '#3E5F65',
+        slateGray: '#637a7f',
+        silver: '#afbdc0',
+        aquamarine: '#04e3bb',
+        lightAquamarine: '#04ECC2',
+        offBlack: '#131320'
+      },
+      spacing: {
+        '7': '1.75rem',
+        '18': '4.5rem'
+      },
+      leading: {
+        '7': '1.8'
+      },
+    }
   },
-  variants: {},
-  plugins: []
 }
